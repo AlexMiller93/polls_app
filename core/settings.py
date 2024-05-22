3a13a16cbda2111f5445dcaa187f3b8d5aebeffe
AlexMiller93 <alexandr.miller93@gmail.com>
2024-05-22 14:36:33 +0300

    add auth, html templates

--- core/settings.py
+++ core/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,8 +121,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = 'users:login'
+LOGIN_REDIRECT_URL = 'app:index'
--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,14 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
+from users.views import SignUp
+
+app_name = "users"
 
 urlpatterns = [
-    path('')
+    path('signup/', SignUp.as_view(), name='signup'),
+    path('login/', LoginView.as_view(
+        template_name='users/login.html'), name='login'),
+    path('auth/logout/', LogoutView.as_view(
+        template_name='users/logged_out.html'), name='logout'),
 ]
